a4a22cfd55408452ffa08d7e08e1be44411a0749
Ashish Sharma <ashishs@ECL.LOCAL>
2020-09-07 18:04:05 +0530

    added file

--- myfirst2file.php
+++ myfirst2file.php
@@ -1,1 +1,22 @@
-ff
+git clone https://github.com/aksharmapbo/myfirst.git
+
+ashishs@LT-PBO-PF09GGAN MINGW64 /c/xampp/htdocs
+$ cd myfirst
+
+ashishs@LT-PBO-PF09GGAN MINGW64 /c/xampp/htdocs/myfirst (master)
+$ git pull
+remote: Enumerating objects: 4, done.
+
+
+ashishs@LT-PBO-PF09GGAN MINGW64 /c/xampp/htdocs/myfirst (master)
+$ git add myfirs1file.php
+
+ashishs@LT-PBO-PF09GGAN MINGW64 /c/xampp/htdocs/myfirst (master)
+$ git status
+
+
+ashishs@LT-PBO-PF09GGAN MINGW64 /c/xampp/htdocs/myfirst (master)
+$ git commit -am "added file"
+
+ashishs@LT-PBO-PF09GGAN MINGW64 /c/xampp/htdocs/myfirst (master)
+$ git push
